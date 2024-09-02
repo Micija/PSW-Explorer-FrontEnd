d12495484e8756e5cd23e24e2270670cc47257b4
Micija <andjelic.ra70.2018@uns.ac.rs>
2024-09-02 17:54:23 +0200

    FEAT: Reports overview implemented

--- src/components/Author/reports-overview/reports-overview.component.ts
+++ src/components/Author/reports-overview/reports-overview.component.ts
@@ -7,11 +7,14 @@ import { ReportService } from '../../../services/report.service';
   styleUrls: ['./reports-overview.component.scss'],
 })
 export class ReportsOverviewComponent implements OnInit {
+  reports: any[] = [];
+
   constructor(private reportService: ReportService) {}
 
   ngOnInit(): void {
     this.reportService.getAllForAuthor().subscribe((reports) => {
       console.log(reports);
+      this.reports = reports;
     });
   }
 }
